9b18740dde5d44f8edb40e0b5193d2ec2aee8330
rjiang8246 <richard.jiang.8246@outlook.com>
2026-06-01 06:57:47 +0100

    Initial project setup

---
+++ src/main/java/rjiang/springboot/auth/SpringbootAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package rjiang.springboot.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rjiang/springboot/auth/SpringbootAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rjiang.springboot.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
